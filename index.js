5efef282599483000b7a92b19d2cb937f886aeb5
Shahriar-Samir <shabusiness035@gmail.com>
2024-06-04 16:17:11 +0600

    added a delete route that removes a specific application from database

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors({
     origin: ['http://localhost:5173','https://active-edge-0.web.app','https://active-edge-0.firebaseapp.com']
 }))
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@databases1.utppk3d.mongodb.net/?retryWrites=true&w=majority&appName=databases1`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -108,6 +108,13 @@ async function run() {
         const application = req.body
         const addApplication = await applicationCollection.insertOne(application)
         res.send(addApplication)
+    })
+
+    app.delete('/deleteApplication/:id',async (req,res)=>{
+          const {id} = req.params
+          const applicationId = new ObjectId(id)
+          const deleteApplication = await applicationCollection.deleteOne({_id:applicationId})
+          res.send(deleteApplication)
     })
    
     // Send a ping to confirm a successful connection
